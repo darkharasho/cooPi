199990a1affbb3acb93f09bdcaa16d46fe537fd9
Michael Stephens <project96@gmail.com>
2024-04-23 20:45:06 -0700

    .

--- lib/pca9685.py
+++ lib/pca9685.py
@@ -1,14 +1,14 @@
 import time
 import math
 import smbus
-from lib import cfg
+from lib import config
 
 
 class PCA9685:
     def __init__(self):
         self.i2c = smbus.SMBus(1)
         self.dev_addr = 0x7f
-        self.write_reg(cfg.MODE1, 0x00)
+        self.write_reg(config.MODE1, 0x00)
 
     def write_reg(self, reg, value):
         self.i2c.write_byte_data(self.dev_addr, reg, value)
@@ -24,20 +24,20 @@ class PCA9685:
         prescaleval -= 1.0
         prescale = math.floor(prescaleval + 0.5)
 
-        oldmode = self.read_reg(cfg.MODE1)
+        oldmode = self.read_reg(config.MODE1)
         print('lodmode:', oldmode)
         newmode = (oldmode & 0x7F) | 0x10  # sleep
-        self.write_reg(cfg.MODE1, newmode)  # go to sleep
+        self.write_reg(config.MODE1, newmode)  # go to sleep
         self.write_reg(PRESCALE, int(math.floor(prescale)))
-        self.write_reg(cfg.MODE1, oldmode)
+        self.write_reg(config.MODE1, oldmode)
         time.sleep(0.005)
-        self.write_reg(cfg.MODE1, oldmode | 0x80)  # 0x80
+        self.write_reg(config.MODE1, oldmode | 0x80)  # 0x80
 
     def setPWM(self, ch, on, off):
-        self.write_reg(cfg.LED0_ON_L + 4 * ch, on & 0xFF)
-        self.write_reg(cfg.LED0_ON_H + 4 * ch, on >> 8)
-        self.write_reg(cfg.LED0_OFF_L + 4 * ch, off & 0xFF)
-        self.write_reg(cfg.LED0_OFF_H + 4 * ch, off >> 8)
+        self.write_reg(config.LED0_ON_L + 4 * ch, on & 0xFF)
+        self.write_reg(config.LED0_ON_H + 4 * ch, on >> 8)
+        self.write_reg(config.LED0_OFF_L + 4 * ch, off & 0xFF)
+        self.write_reg(config.LED0_OFF_H + 4 * ch, off >> 8)
 
     def setServoPulse(self, channel, pulse):
         pulse = pulse * 4096 / 20000  # PWM frequency is 50HZ,the period is 20000us=20ms
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from lib.cfg import *
+from lib import config
 from lib.pca9685 import PCA9685
 from celery import Celery
 
@@ -32,17 +32,17 @@ if __name__ == '__main__':
         """The following code is applied to
         DC motor control
         """
-        pwm.setServoPulse(cfg.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
+        pwm.setServoPulse(config.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
         # CCW
-        pwm.setServoPulse(cfg.DC_MOTOR_INA1, 0)  # set INA1 L
-        pwm.setServoPulse(cfg.DC_MOTOR_INA2, 19999)  # set INA2 H
+        pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 L
+        pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 H
         print("M1 rotate")
         time.sleep(2)
         # CW
-        pwm.setServoPulse(cfg.DC_MOTOR_INA1, 19999)  # set INA1 H
-        pwm.setServoPulse(cfg.DC_MOTOR_INA2, 0)  # set INA2 L
+        pwm.setServoPulse(config.DC_MOTOR_INA1, 19999)  # set INA1 H
+        pwm.setServoPulse(config.DC_MOTOR_INA2, 0)  # set INA2 L
         print("M1 rotate opposite")
         time.sleep(2)
-        pwm.setServoPulse(cfg.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
+        pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
         print("M1 stop")
         time.sleep(2)
