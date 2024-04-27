fe9d65db55ec5519021e6340822772bd90008aa3
Michael Stephens <project96@gmail.com>
2024-04-27 15:51:45 -0700

    test sched

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ from lib import config
 from lib.pca9685 import PCA9685
 
 
+def door_control(status: str):
+    pwm = PCA9685()
+    pwm.setPWMFreq(50)  # for servo
+
+    print("Door control activated")
+    pwm.setServoPulse(config.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
+
+    if status == 'open':
+        pwm.setServoPulse(config.DC_MOTOR_INA1, 19999)  # set INA1 L
+        pwm.setServoPulse(config.DC_MOTOR_INA2, 0)  # set INA2 H
+        print("M1 rotate")
+        time.sleep(8)
+        pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)
+    elif status == 'close':
+        pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 H
+        pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 L
+        print("M1 rotate opposite")
+        time.sleep(8)
+        pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
+    else:
+        print("Invalid status")
+
 def open_door():
     pwm = PCA9685()
     pwm.setPWMFreq(50)  # for servo
@@ -26,6 +48,8 @@ def close_door():
     pwm.setPWMFreq(50)  # for servo
 
     # CW
+    pwm.setServoPulse(config.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
+
     pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 H
     pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 L
     print("M1 rotate opposite")
@@ -52,16 +76,14 @@ if __name__ == '__main__':
     L     L    H    H    OFF    OFF      STOP
     H/L   H/L  H/L   L    OFF    OFF    standby
     """
-    pwm = PCA9685()
-    pwm.setPWMFreq(50)  # for servo
     close_door()
 
-    schedule.every().day.at("15:47:00").do(open_door)
-    schedule.every().day.at("15:48:00").do(close_door)
+    schedule.every().day.at("15:53:00").do(door_control("open"))
+    schedule.every().day.at("15:54:00").do(door_control("close"))
 
-    schedule.every().day.at("07:00").do(open_door)
+    # schedule.every().day.at("06:00").do(open_door)
 
-    schedule.every().day.at("20:00").do(close_door)
+    # schedule.every().day.at("21:00").do(close_door)
 
     try:
         while True:
