965316d95c98d674b95dae1cfe6458c8b9faae00
Michael Stephens <project96@gmail.com>
2024-04-27 15:27:16 -0700

    test schedule

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ if __name__ == '__main__':
         DC motor control
         """
 
-        schedule.every().day.at("15:27").do(open)
+        schedule.every().day.at("15:28").do(open)
 
-        schedule.every().day.at("7:00").do(open)
+        schedule.every().day.at("07:00").do(open)
 
         schedule.every().day.at("20:00").do(close)
