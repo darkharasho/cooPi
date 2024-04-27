5638abe77fdfe93d8ca4cae12ac818bcd555c94e
Michael Stephens <project96@gmail.com>
2024-04-27 15:16:21 -0700

    methodize open and close

--- main.py
+++ main.py
@@ -3,8 +3,35 @@ from lib import config
 from lib.pca9685 import PCA9685
 from celery import Celery
 
-
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+
+def open():
+    pwm = PCA9685()
+    pwm.setPWMFreq(50)  # for servo
+
+    print("open")
+    pwm.setServoPulse(config.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
+    # CCW
+    pwm.setServoPulse(config.DC_MOTOR_INA1, 19999)  # set INA1 L
+    pwm.setServoPulse(config.DC_MOTOR_INA2, 0)  # set INA2 H
+    print("M1 rotate")
+    time.sleep(8)
+
+
+def close():
+    print("close")
+
+    pwm = PCA9685()
+    pwm.setPWMFreq(50)  # for servo
+
+    # CW
+    pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 H
+    pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 L
+    print("M1 rotate opposite")
+    time.sleep(8)
+    pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
+
 
 if __name__ == '__main__':
     """for servo motor:
@@ -32,18 +59,7 @@ if __name__ == '__main__':
         """The following code is applied to
         DC motor control
         """
-        pwm.setServoPulse(config.DC_MOTOR_PWM1, 15000)  # for TB6612 set speed
-        # CCW
-        pwm.setServoPulse(config.DC_MOTOR_INA1, 19999)  # set INA1 L
-        pwm.setServoPulse(config.DC_MOTOR_INA2, 0)  # set INA2 H
-        print("M1 rotate")
-        time.sleep(8)
-        # CW
-        pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 H
-        pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 L
-        print("M1 rotate opposite")
-        time.sleep(8)
-        pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
-        print("M1 stop")
-        time.sleep(2)
+
+        open()
+        close()
         exit()
