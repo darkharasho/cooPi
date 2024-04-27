89a2db8a810d221e67c0480c8f8b788113c5a736
Michael Stephens <project96@gmail.com>
2024-04-27 15:09:17 -0700

    test #5

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ if __name__ == '__main__':
         pwm.setServoPulse(config.DC_MOTOR_INA1, 19999)  # set INA1 L
         pwm.setServoPulse(config.DC_MOTOR_INA2, 0)  # set INA2 H
         print("M1 rotate")
-        time.sleep(7)
+        time.sleep(8)
         # CW
         pwm.setServoPulse(config.DC_MOTOR_INA1, 0)  # set INA1 H
         pwm.setServoPulse(config.DC_MOTOR_INA2, 19999)  # set INA2 L
         print("M1 rotate opposite")
-        time.sleep(7)
+        time.sleep(8)
         pwm.setServoPulse(config.DC_MOTOR_PWM1, 0)  # for TB6612 set speed to 0, stop
         print("M1 stop")
         time.sleep(2)
